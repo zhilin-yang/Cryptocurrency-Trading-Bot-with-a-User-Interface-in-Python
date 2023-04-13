8e47a19a21c410bc3149cd3f54d5fa523e221d9d
Becominging <1312545407@qq.com>
2023-04-13 17:58:07 +0100

    change UI color to ireland national flag

--- interface/styling.py
+++ interface/styling.py
@@ -2,9 +2,9 @@
 Define global styling variables here that can be used in multiple components.
 """
 
-BG_COLOR = "gray12"
-BG_COLOR_2 = "#1c2c5c"
-FG_COLOR = "white"
-FG_COLOR_2 = "SteelBlue1"
+BG_COLOR = "white"  #"white"
+BG_COLOR_2 = "#169B62"  # "#1c2c5c"is blue change to green
+FG_COLOR = "#FF7900" #"gray12" to orange
+FG_COLOR_2 = "#FF7900"  # "SteelBlue1"change to orange
 GLOBAL_FONT = ("Calibri", 11, "normal")
 BOLD_FONT = ("Calibri", 11, "bold")
